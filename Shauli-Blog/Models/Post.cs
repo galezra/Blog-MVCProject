7f8d7453755dca574281532780d3e23c7244cd9a
galezra <galezra@gmail.com>
2017-05-31 17:06:26 +0300

    First commit

---
+++ Shauli-Blog/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Shauli_Blog.Models;
+
+namespace Shauli_Blog
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Content { get; set; }
+        public string Url { get; set; }
+        public string Image { get; set; }
+        public string Video { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
