0d424abb1793b4ee6c6eaa042bda29ee02faf50c
galezra <galezra@gmail.com>
2017-05-31 18:20:24 +0300

    Add Fan controller

---
+++ Shauli-Blog/Controllers/FanController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Shauli_Blog.DAL;
+using Shauli_Blog.Models;
+
+namespace Shauli_Blog.Controllers
+{
+    public class FanController : Controller
+    {
+        private BlogContex db = new BlogContex();
+
+        // GET: Fan
+        public ActionResult Index()
+        {
+            return View(db.Fans.ToList());
+        }
+
+        // GET: Fan/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fan fan = db.Fans.Find(id);
+            if (fan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fan);
+        }
+
+        // GET: Fan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fan/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FanId,FName,LName,Genre,BDate,Seniority")] Fan fan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Fans.Add(fan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(fan);
+        }
+
+        // GET: Fan/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fan fan = db.Fans.Find(id);
+            if (fan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fan);
+        }
+
+        // POST: Fan/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FanId,FName,LName,Genre,BDate,Seniority")] Fan fan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(fan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fan);
+        }
+
+        // GET: Fan/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Fan fan = db.Fans.Find(id);
+            if (fan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fan);
+        }
+
+        // POST: Fan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Fan fan = db.Fans.Find(id);
+            db.Fans.Remove(fan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
