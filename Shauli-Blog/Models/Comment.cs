274f4577f6c01c7907f0fde30e9462d778e3dc03
galezra <galezra@gmail.com>
2017-05-31 18:25:04 +0300

    Fix change.

--- Shauli-Blog/Models/Comment.cs
+++ Shauli-Blog/Models/Comment.cs
@@ -12,9 +12,9 @@ namespace Shauli_Blog.Models
         public int CommentId { get; set; }
         public int PostId { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
         public string Author { get; set; }
         public string Url { get; set; }
+        public string Content { get; set; }
         public virtual Post Post { get; set; }
     }
 }
