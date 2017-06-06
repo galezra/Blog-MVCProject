80bda7fbaa29a05d74e63441da28112e04eb08ef
galezra <galezra@gmail.com>
2017-06-06 20:29:20 +0300

    First commit

---
+++ Shauli-Blog/Scripts/Post/post_create.js
@@ -0,0 +1,19 @@
+﻿$('#addImageCheckbox').click(function () {
+    var $this = $(this);
+
+    if ($this.is(':checked')) {
+        $('#image').removeAttr("disabled");
+    } else {
+        $('#image').attr("disabled", "disabled")
+    }
+});
+
+$('#addVideoCheckbox').click(function () {
+    var $this = $(this);
+
+    if ($this.is(':checked')) {
+        $('#video').removeAttr("disabled");
+    } else {
+        $('#video').attr("disabled", "disabled")
+    }
+});
